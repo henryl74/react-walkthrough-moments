4f81755f4ff164bf9a42506a6542e48a4ab0bfda
Henry Leon <henryatusabes@gmail.com>
2023-03-06 15:07:38 +0000

    Added Authentication to create the SignUp form part 1

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://django-rest-api-project.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
